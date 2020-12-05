3a0628dea7a4843a7fcbbc8bd504d0b693476290
edward-hong <edward.hong527@gmail.com>
2020-12-06 01:58:53 +1300

    Container component with slide animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
-const App = () => <p>Hello World</p>
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border-radius: 50px;
+  box-shadow: 3px -8px 71px 0px rgba(0, 0, 0, 0.75);
+  margin: 0;
+  position: fixed;
+  top: ${({ top }) => top}%;
+  transition: top 1s;
+`
+
+const App = () => {
+  const [top, setTop] = useState(85)
+
+  return (
+    <Container top={top} onClick={() => setTop(30)}>
+      {''}
+    </Container>
+  )
+}
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
